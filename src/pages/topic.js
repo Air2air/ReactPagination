eab6620a9623d8793c2b389032f05c1f46abf877
Air2air <todd@dunningmarketing.com>
2021-05-27 15:47:00 -0700

    Works with cleanup

--- src/pages/topic.js
+++ src/pages/topic.js
@@ -1,23 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { useHistory } from 'react-router-dom';
-import ReactPaginate from "react-paginate";
 import Layout from "../components/layout/layout";
 import { Row, Col, Button } from "react-bootstrap";
 
-//import "./App.css";
 
 function Topic() {
-
-
-  const history = useHistory();
-
-
-
-
   const [currentPage, setCurrentPage] = useState(0);
   const [profiles, setProfile] = useState([]);
-  const singleUserProfile = useState();
-  const [home_button, show_home_button] = useState(false);
 
   const dataSource = "./data/topics.json";
 
@@ -32,41 +20,23 @@ function Topic() {
   const PER_PAGE = 1;
   const offset = currentPage * PER_PAGE;
   const currentPageData = profiles.slice(offset, offset + PER_PAGE);
-  const pageCount = Math.ceil(profiles.length / PER_PAGE);
-  const foundPageData = singleUserProfile.slice(offset, offset + PER_PAGE);
 
-  // function handlePageClick({ selected: selectedPage }) {
-  //   setCurrentPage(selectedPage);
-  // }
-
-  function handlePageClick({ selected: selectedPage }) {
-    setCurrentPage(currentPage + 1);
+  function handlePrevClick() {
+    setCurrentPage(currentPage - 1);
   }
 
-  let japaToHome = () => {
-    setProfile(profiles);
-    show_home_button(false);
-  };
-
-  let UserProfile = home_button ? foundPageData : currentPageData;
-
-  let navigateToHome = home_button ? (
-    <button onClick={japaToHome}>All Users</button>
-  ) : (
-    ""
-  );
-
-  let buttonLabelPrevious = "Prev";
-  let buttonLabelNext = "Next";
+  function handleNextClick() {
+    setCurrentPage(currentPage + 1);
+  }
 
   return (
     <Layout>
       <Row>
         <Col xs lg="2">
-          {navigateToHome}
+          {/* {navigateToHome} */}
         </Col>
         <Col md="auto">
-          {UserProfile.map((profile) => {
+          {currentPageData.map((profile) => {
             const { topicId, category, gender, content } = profile;
             return (
               <Col key={topicId}>
@@ -76,16 +46,17 @@ function Topic() {
             );
           })}
         </Col>
-        <Col xs lg="2">
-          3 of 3
-        </Col>
+        <Col xs lg="2"></Col>
       </Row>
       <Row>
-      <div id="paginate">
-
-      <Button variant="btn btn-success" onClick={(handlePageClick)}>Click </Button>
-
-      </div>
+        <div id="paginate">
+          <Button variant="btn btn-success" onClick={handlePrevClick}>
+            PrevClick{" "}
+          </Button>
+          <Button variant="btn btn-success" onClick={handleNextClick}>
+            NextClick{" "}
+          </Button>
+        </div>
       </Row>
     </Layout>
   );
